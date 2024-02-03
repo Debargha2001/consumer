1fea947f43ccbc2c4f8b776ee119b3b857fd1ad1
Debargha Dutta <debarghadutta2001@gmail.com>
2024-02-03 14:56:05 +0530

    rooms added

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const http = require("http");
-const {Server} = require("socket.io")
-const excel = require("exceljs")
+const { Server } = require("socket.io");
+const excel = require("exceljs");
 const {
   SQSClient,
   GetQueueUrlCommand,
   ReceiveMessageCommand,
-  DeleteMessageCommand
+  DeleteMessageCommand,
 } = require("@aws-sdk/client-sqs");
 
 const app = express();
@@ -15,17 +15,33 @@ const server = http.createServer(app);
 const io = new Server(server, {
   serveClient: false,
   cors: {
-    origin: "*"
+    origin: "*",
   },
 });
 
+// const socketMiddleware = async (socket, next) => {
+//   const deviceId = socket.handshake.query.deviceId;
+//   if (!deviceId) {
+//     return next(new Error("no device id found"));
+//   }
+//   return next();
+// };
+
+// io.use(socketMiddleware);
 
 io.on("connection", (socket) => {
   console.log("connected to socket client");
+  const deviceId = socket.handshake.query.deviceId;
+  if (deviceId) {
+    socket.join(String(deviceId));
+    console.log(`Rooms => ${socket.rooms.has(deviceId)}`);
+    console.log("joining room => ", deviceId);
+  }
+
   socket.on("receiveQueueMessage", async (data) => {
-    console.log(data)
+    console.log(data);
     await pollMessages();
-  })
+  });
   socket.on("disconnect", () => {
     console.log("socket client disconnected");
   });
@@ -41,75 +57,79 @@ const client = new SQSClient({
 app.use(express.json());
 
 function transformData(data) {
-    if (!data) {
-      return;
-    }
-  
-    const headers = data[1]; // Ensure headers are an array
-    const dataRows = data.slice(2);
-  
-    const transformedData = dataRows.map((row) => {
-      const object = {};
-      for (let i = 1; i < headers.length; i++) {
-        if (row && Array.isArray(row)) {  // Check for null/undefined and array type
-          object[`${headers[i]}`] = row[i];
-        } else {
-          object[`${headers[i]}`] = null;  // Assign null for missing values
-        }
-      }
-      return object;
-    });
-  
-    return transformedData;
+  if (!data) {
+    return;
   }
 
-async function processFileData(data) {
+  const headers = data[1]; // Ensure headers are an array
+  const dataRows = data.slice(2);
 
-    const workbook = new excel.Workbook();
-      await workbook.xlsx.load(data);
-      const result = [];
-      workbook.eachSheet((sheet, sheetId) => {
-        const convertedData = sheet.getSheetValues();
-        result.push(transformData(convertedData));
-      });
-      return result;
+  const transformedData = dataRows.map((row) => {
+    const object = {};
+    for (let i = 1; i < headers.length; i++) {
+      if (row && Array.isArray(row)) {
+        // Check for null/undefined and array type
+        object[`${headers[i]}`] = row[i];
+      } else {
+        object[`${headers[i]}`] = null; // Assign null for missing values
+      }
+    }
+    return object;
+  });
+
+  return transformedData;
+}
+
+async function processFileData(data) {
+  const workbook = new excel.Workbook();
+  await workbook.xlsx.load(data);
+  const result = [];
+  workbook.eachSheet((sheet, sheetId) => {
+    const convertedData = sheet.getSheetValues();
+    result.push(transformData(convertedData));
+  });
+  return result;
 }
 
 async function pollMessages() {
-    console.log("listening for producer message")
+  console.log("listening for producer message");
   try {
-    // while (true) {
-      const getQueueUrlCommand = new GetQueueUrlCommand({
-        QueueName: "file-upload-test-queue",
+    const getQueueUrlCommand = new GetQueueUrlCommand({
+      QueueName: "file-upload-test-queue",
+    });
+    const { QueueUrl } = await client.send(getQueueUrlCommand);
+    console.log(QueueUrl);
+    const recieveMessagCommand = new ReceiveMessageCommand({
+      QueueUrl: QueueUrl,
+      MaxNumberOfMessages: 1,
+    });
+    const { Messages } = await client.send(recieveMessagCommand);
+    if (Messages && Messages.length > 0) {
+      const { Body, ReceiptHandle } = Messages[0];
+      // console.log(`Body: ${JSON.stringify(Body)}`)
+      const fileData = Buffer.from(JSON.parse(Body).fileData, "base64");
+      const deviceId = JSON.parse(Body).deviceId;
+      console.log(`Device-id: ${deviceId}`);
+
+      const result = await processFileData(fileData);
+      const deleteMessageCommand = new DeleteMessageCommand({
+        QueueUrl,
+        ReceiptHandle,
       });
-      const { QueueUrl } = await client.send(getQueueUrlCommand);
-      console.log(QueueUrl);
-      const recieveMessagCommand = new ReceiveMessageCommand({
-        QueueUrl: QueueUrl,
-        MaxNumberOfMessages: 1,
+      await client.send(deleteMessageCommand);
+      return new Promise((resolve) => {
+        io.to(deviceId).emit("parse:excel", result, (err, resp) => {
+          console.log(`event emitted to room ${deviceId}\n`, result);
+          if (err) {
+            return resolve({
+              error: true,
+              message: err?.message ?? "something went wrong",
+            });
+          }
+
+          return resolve(resp[0] ?? resp);
+        });
       });
-      const {Messages} = await client.send(recieveMessagCommand);
-      if(Messages && Messages.length > 0){
-        const {Body, ReceiptHandle} = Messages[0];
-        const fileData = Buffer.from(JSON.parse(Body).fileData, "base64")
-        const result = await processFileData(fileData);
-        const deleteMessageCommand = new DeleteMessageCommand({QueueUrl,ReceiptHandle});
-        await client.send(deleteMessageCommand);
-        return new Promise((resolve) => {
-          io.emit('parse:excel', result, (err, resp) => {
-            console.log(result)
-            if (err) {
-              return resolve({
-                error: true,
-                message: err?.message ?? 'something went wrong'
-              });
-            }
-    
-            return resolve(resp[0] ?? resp);
-          });
-        })
-      // }
-      
     }
   } catch (err) {
     console.log(err);
